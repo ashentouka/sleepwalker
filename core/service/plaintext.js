d5d89f9d1907600c03cab6e166775be89409fc9f
Zoey Ashlynn <ashentouka@gmail.com>
2024-08-11 06:59:08 -0400

    hotfix service

---
+++ core/service/plaintext.js
@@ -0,0 +1,10 @@
+module.exports = function(req, res, next){
+  if (req.is('text/*')) {
+    req.text = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk){ req.text += chunk });
+    req.on('end', next);
+  } else {
+    next();
+  }
+}
